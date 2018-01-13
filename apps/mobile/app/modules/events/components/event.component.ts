344eae143dbadd1e7f8fb5fdf41ecba689983211
Nathan Walker <nwalker@nstudio.io>
2018-01-12 21:30:03 -0800

    fix(iOS): plist

--- apps/mobile/app/modules/events/components/event.component.ts
+++ apps/mobile/app/modules/events/components/event.component.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngrx/store';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
-import { CalendarViewMode, CalendarTransitionMode, CalendarEvent, CalendarEventsViewMode } from 'nativescript-pro-ui/calendar';
+// import { CalendarViewMode, CalendarTransitionMode, CalendarEvent, CalendarEventsViewMode } from 'nativescript-pro-ui/calendar';
 import { Color } from 'tns-core-modules/color';
 import { View } from 'tns-core-modules/ui/core/view';
 import { isIOS, platformNames, device } from 'tns-core-modules/platform';
@@ -39,10 +39,10 @@ export class EventComponent extends BaseComponent implements AfterViewInit, OnIn
 
   public eventState$: Observable<any>;
   public locale = 'en-US';
-  public viewMode = CalendarViewMode.Month;
-  public transitionMode = CalendarTransitionMode.Stack;
+  // public viewMode = CalendarViewMode.Month;
+  // public transitionMode = CalendarTransitionMode.Stack;
   public eventSource = [];
-  public eventsViewMode = CalendarEventsViewMode.Inline;
+  // public eventsViewMode = CalendarEventsViewMode.Inline;
   public checkboxValue = false;
   private _checkbox: CheckBox;
   private _listview: ListView;
@@ -176,11 +176,11 @@ export class EventComponent extends BaseComponent implements AfterViewInit, OnIn
   }
 
   public changeView( type: 'day' | 'week' ) {
-    if ( type === 'day' ) {
-      this.viewMode = CalendarViewMode.Day;
-    } else {
-      this.viewMode = CalendarViewMode.Week;
-    }
+    // if ( type === 'day' ) {
+    //   this.viewMode = CalendarViewMode.Day;
+    // } else {
+    //   this.viewMode = CalendarViewMode.Week;
+    // }
   }
 
   public checkboxLoaded( e ) {
@@ -243,13 +243,13 @@ export class EventComponent extends BaseComponent implements AfterViewInit, OnIn
     }
   }
 
-  public getCalendarEvents(): Array<CalendarEvent> {
+  public getCalendarEvents(): Array<any> {//<CalendarEvent> {
     let now = new Date();
     let startDate: Date,
       endDate: Date,
-      event: CalendarEvent;
+      event: any;// CalendarEvent;
     let colors: Array<Color> = [new Color( 200, 188, 26, 214 ), new Color( 220, 255, 109, 130 ), new Color( 255, 55, 45, 255 ), new Color( 199, 17, 227, 10 ), new Color( 255, 255, 54, 3 )];
-    let events: Array<CalendarEvent> = [];
+    let events: Array<any> = [];//<CalendarEvent> = [];
     for ( let i = 0; i < 4; i++ ) {
       let day;
       let month = 0;
@@ -271,12 +271,12 @@ export class EventComponent extends BaseComponent implements AfterViewInit, OnIn
       }
       startDate = new Date( 2018, month, day - 1, 1 );
       endDate = new Date( 2018, month, day - 1, 3 );
-      event = new CalendarEvent( "event " + i, startDate, endDate, false, colors[i * 10 % ( colors.length - 1 )] );
-      events.push( event );
-      if ( i % 3 == 0 ) {
-        event = new CalendarEvent( "second " + i, startDate, endDate, true, colors[i * 5 % ( colors.length - 1 )] );
-        events.push( event );
-      }
+      // event = new CalendarEvent( "event " + i, startDate, endDate, false, colors[i * 10 % ( colors.length - 1 )] );
+      // events.push( event );
+      // if ( i % 3 == 0 ) {
+      //   event = new CalendarEvent( "second " + i, startDate, endDate, true, colors[i * 5 % ( colors.length - 1 )] );
+      //   events.push( event );
+      // }
     }
     return events;
   }
