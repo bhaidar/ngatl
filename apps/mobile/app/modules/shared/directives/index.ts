b199ed9f5051513932827bcbb5ec95da48b789e2
Josh Sommer <JoshDSommer@gmail.com>
2018-01-14 09:42:37 -0500

    adding collapse directive to shared module

--- apps/mobile/app/modules/shared/directives/index.ts
+++ apps/mobile/app/modules/shared/directives/index.ts
@@ -1,5 +1,7 @@
 import { TouchColorDirective } from './touch-color.directive';
+import { CollapseDirective } from './collapse.directive';
 
 export const SHARED_DIRECTIVES = [
-  TouchColorDirective
+  TouchColorDirective,
+  CollapseDirective
 ];
