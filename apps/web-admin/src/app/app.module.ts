f9e49ad57fac320cffc96b7c7615519841b41e82
Bram Borggreve <borggreve@gmail.com>
2018-12-10 01:26:29 -0500

    feat(web-admin): add material design

--- apps/web-admin/src/app/app.module.ts
+++ apps/web-admin/src/app/app.module.ts
@@ -9,11 +9,12 @@ import { SharedModule } from './features/shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const routes: Routes = [];
 
 @NgModule({
-  imports: [CoreModule, SharedModule, RouterModule.forRoot(routes)],
+  imports: [CoreModule, SharedModule, RouterModule.forRoot(routes), BrowserAnimationsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
